fd5a77ed849098405c63a630ee289e7ab671f970
Vu Minh <minhvh99@gmail.com>
2018-07-26 16:05:37 +0700

    add vector functions, split input string, correct function linkIndex()

--- Source Files/Source Files/Auxilary Functions.cpp
+++ Source Files/Source Files/Auxilary Functions.cpp
@@ -18,8 +18,89 @@ int linkIndex(char x) {
 	else if (isdigit(x))
 		return x - '0' + 26;
 	else if (x == '$')
-		return 26;
+		return 36;
 	//meaning x=='#'
 	else
-		return 27;
+		return 37;
+}
+
+void operator+= (vector<Node> &v1, const vector<Node> &v2) {
+	vector<Node>::const_iterator i;
+	for (i = v2.begin(); i != v2.end(); ++i)
+		v1.push_back(*i);
+}
+
+vector<Node> merge(const vector<Node> &v1, const vector<Node> &v2) {
+	vector<Node> result;
+	result += substract(v1, v2);
+	result += substract(v2, v1);
+	result += intersect(v1, v2);
+	return result;
+}
+
+vector<Node> substract (const vector<Node> &v1, const vector<Node> &v2) {
+	vector<Node>::const_iterator i, j;
+	vector<Node> result;
+	for (i = v1.begin(); i != v1.end(); ++i) {
+		bool isInBothVector = false;
+		for (j = v2.begin(); j != v2.end(); ++j)
+			if ((*i).file_name == (*j).file_name) {
+				isInBothVector = true;
+				break;
+			}
+		if (!isInBothVector)
+			result.push_back(*i);
+	}
+	return result;	
+}
+
+vector<Node> intersect(const vector<Node> &v1, const vector<Node> &v2) {
+	vector<Node>::const_iterator i, j;
+	vector<Node> result;
+	for (i = v1.begin(); i != v1.end(); ++i) {
+		int totalKeywordCount = (*i).keyword_count;
+		for (j = v2.begin(); j != v2.end(); ++j)
+			if ((*i).file_name == (*j).file_name)
+				totalKeywordCount += (*j).keyword_count;
+		result.push_back(Node(totalKeywordCount, (*i).file_name));
+	}
+	return result;
+}
+
+void splitString(string inputStr, vector<string> &tokens, vector<string> &exactMatch) {
+	stringstream ss(inputStr);
+	string tmp;
+	vector<string> t;
+	for (int i = 0; getline(ss, tmp, '\"'); ++i) {
+		if (i % 2 != 0) {
+			exactMatch.push_back(tmp);
+		}
+		else {
+			stringstream sss(tmp);
+			string temp;
+			while (sss >> temp)
+				tokens.push_back(temp);
+		}
+	}
+}
+
+vector<Node> Trie_t::getData(string keyword) {
+	// TO DO: wildcard character (*)
+	if (keyword.size() >= 1 && (keyword[0] == '+' || keyword[0] == '-'))
+		return search(keyword.substr(1, string::npos))->file_list;
+	if (keyword.size() >= strlen("intitle:") && keyword.substr(0, 8) == "intitle:")
+		return search(keyword.substr(8, string::npos))->title_list;
+	if (keyword.size() >= 1 && keyword[0] == '~') {
+		vector<Node> ans;
+		vector<string> syn = synonyms(keyword.substr(1, string::npos));
+		int n = syn.size();
+		for (int i = 0; i < n; ++i)
+			ans = merge(ans, getData(syn[i]));
+		return ans;
+	}
+	return search(keyword)->file_list;
+}
+
+vector<string> synonyms(string word) {
+
 }
--- Source Files/Source Files/Function.h
+++ Source Files/Source Files/Function.h
@@ -42,7 +42,16 @@ public:
 	Word_t* insert(string word);
 	Word_t* search(string word);
 	void inputFromFile(string folder_path);
+<<<<<<< HEAD
+	void minus(string word_not_in_operator, string word_in_operator);
+	
+	// return data of a keyword
+	vector<Node> getData(string keyword);
+	// return if s is a stopword or not
+	bool isStopWord(string s);
+=======
 	vector<int> minus(string word_not_in_operator, string word_in_operator);
+>>>>>>> c2fd3ced93d6e42e6f99a8dc47980604c4bfe2e2
 private:
 	Word_t * root = NULL;
 };
@@ -52,6 +61,18 @@ string itoXX(int number);
 bool NodeMaxFirst(Node a, Node b);
 int linkIndex(char x);
 
+<<<<<<< HEAD
+// vector functions
+void operator+= (vector<Node> &v1, const vector<Node> &v2); // add two vectors
+vector<Node> merge(const vector<Node> &v1, const vector<Node> &v2); // return nodes in v1 or v2
+vector<Node> substract (const vector<Node> &v1, const vector<Node> &v2); // return nodes in v1 but not in v2
+vector<Node> intersect(const vector<Node> &v1, const vector<Node> &v2); // return nodes in both v1 and v2
+
+// string functions
+void splitString(string inputStr, vector<string> &tokens, vector<string> &exactMatch); // split intputStr into tokens
+vector<string> synonyms(string word); // return all synonyms of word
+
+=======
 bool exist(vector<Node> v, int file_name) {
 	vector<Node>::iterator it;
 	for (it = v.begin(); it != v.end(); ++it) {
@@ -60,5 +81,6 @@ bool exist(vector<Node> v, int file_name) {
 	}
 	return false;
 }
+>>>>>>> c2fd3ced93d6e42e6f99a8dc47980604c4bfe2e2
 
 #endif // !_FUNCTION_H_
